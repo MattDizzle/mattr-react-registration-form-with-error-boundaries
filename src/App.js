dd8442bdf20ffa7f4ad082ff68297a0d38ffb40e
Matthew Rougely <matthewrougelysr@gmail.com>
2020-07-25 13:33:05 -0700

    Complete with Error Boundaries

--- src/App.js
+++ src/App.js
@@ -5,19 +5,39 @@ import Header from './Header';
 import Form from './Form';
 
 export default class App extends Component {
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-       
-    }
-  }
+  // constructor(props) {
+  //   super(props)
+  //   this.firstNameInput = React.createRef();
+  //   this.lastNameInput = React.createRef();
+  //   this.emailInput = React.createRef();
+  //   this.dobInput = React.createRef();
+  //   this.passwordInput = React.createRef();
+  //   this.repeatPasswordInput = React.createRef();
+  // }
+
+  // handleSubmit(event) {
+  //   event.preventDefault();
+  //   const firstName = this.firstNameInput.current.value;
+  //   const lastName = this.lastNameInput.current.value;
+  //   const email = this.emailInput.current.value;
+  //   const dob = this.dobInput.current.value;
+  //   const password = this.passwordInput.value;
+  //   const repeatPassword = this.repeatPassword;
+
+  //   console.log('firstName:', firstName);
+  //   console.log('lastName:', lastName);
+  //   console.log('email:', email);
+  //   console.log('dob:', dob);
+  //   console.log('password:', password)
+  //   console.log('repeatPassword:', repeatPassword)
+
+  // }
   
   render() {
     return (
       <div>
         <Header />
-        <Form />
+        <Form onSubmit={this.handleSubmit} />
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Form from './Form';
 
 
 ReactDOM.render(
-    <App />,
+    <Form />,
   document.getElementById('root')
 );
 
